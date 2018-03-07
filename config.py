f3d364d1ae026a9d7463d495e4467f3fc7548f23
Jasmine Lo <jasmineelizalo@gmail.com>
2018-03-07 11:14:24 -0600

    in progress

---
+++ config.py
@@ -0,0 +1,6 @@
+# Twitter API Keys
+consumer_key = "PKQK1IXOsnSxcv7LEuejMMVFM"
+consumer_secret = "dVUQcsUlKw4uC5KLLDuK9FUtgCWadzCJzx9eqgOUjSh83oG8AM"
+
+access_token = "967430128237244421-WDEZ5qvwMFDWlFSRFFuKFivITDATdpn"
+access_token_secret = "4tVHJVVvDr8vtiq1Vh4Bsp5pfK6aqqsg71CUHzu3zjAkl"
